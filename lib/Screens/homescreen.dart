50c431382435cfb3bc2ad9c78b19d50ca4df457a
Marya Baloch <maryabaloch7@gmail.com>
2022-07-25 18:43:27 +0500

    Basic Tiles added

--- lib/Screens/homescreen.dart
+++ lib/Screens/homescreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/tiles.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -15,75 +17,140 @@ class HomeScreen extends StatelessWidget {
           size: 32,
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-              height: 400,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(100),
-                  bottomRight: Radius.circular(100),
-                ),
-                image: DecorationImage(
-                    image: AssetImage('assets/images/logoFlutter-01.jpg'),
-                    fit: BoxFit.fill),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      "INTRODUCTION TO",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 38,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "FLUTTER AND DART",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 38,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "Get up to speed about what Flutter is and",
-                      style: TextStyle(
-                        color: Colors.white38,
-                        fontSize: 18,
-                        // fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "how the core concepts of Flutter and Dart",
-                      style: TextStyle(
-                        color: Colors.white38,
-                        fontSize: 18,
-                        // fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "work together.",
-                      style: TextStyle(
-                        color: Colors.white38,
-                        fontSize: 18,
-                        // fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 20)
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+                height: 400,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(150),
+                    bottomRight: Radius.circular(150),
+                  ),
+                  boxShadow: [
+                    BoxShadow(blurRadius: 30, color: Colors.black),
                   ],
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/logoFlutter-01.jpg'),
+                      fit: BoxFit.fill),
                 ),
-              )),
-          const SizedBox(height: 20),
-        ],
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "INTRODUCTION TO",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 38,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "FLUTTER AND DART",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 38,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Get up to speed about what Flutter is and",
+                        style: TextStyle(
+                          color: Colors.white38,
+                          fontSize: 18,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "how the core concepts of Flutter and Dart",
+                        style: TextStyle(
+                          color: Colors.white38,
+                          fontSize: 18,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "work together.",
+                        style: TextStyle(
+                          color: Colors.white38,
+                          fontSize: 18,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20)
+                    ],
+                  ),
+                )),
+            const SizedBox(height: 40),
+            const Text(
+              "Basics",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20.0),
+              height: 160.0,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 20.0,
+                  ),
+                ],
+              ),
+              child: ListView(
+                // This next line does the trick.
+                scrollDirection: Axis.horizontal,
+                children: const <Widget>[
+                  Tiles(
+                    title: "Loops",
+                    color: Colors.orange,
+                    image: "loop",
+                    description: "DART",
+                  ),
+                  SizedBox(width: 10.0),
+                  Tiles(
+                    title: "Loops",
+                    color: Colors.orange,
+                    image: "loop",
+                    description: "DART",
+                  ),
+                  SizedBox(width: 10.0),
+                  Tiles(
+                    title: "Loops",
+                    color: Colors.orange,
+                    image: "loop",
+                    description: "DART",
+                  ),
+                  SizedBox(width: 10.0),
+                  Tiles(
+                    title: "Loops",
+                    color: Colors.orange,
+                    image: "loop",
+                    description: "DART",
+                  ),
+                  SizedBox(width: 10.0),
+                  Tiles(
+                    title: "Loops",
+                    color: Colors.orange,
+                    image: "loop",
+                    description: "DART",
+                  ),
+                  SizedBox(width: 10.0),
+                  SizedBox(width: 10.0),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
